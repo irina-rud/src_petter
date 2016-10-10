e704893a77cc38ce183374dfc43b531ac876e574
obabichev <babichev.oleg.n@gmail.com>
2016-10-10 18:44:17 +0300

    vk auth

--- application/urls.py
+++ application/urls.py
@@ -49,4 +49,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
+    url(r'^social/', include('social_auth.urls')),
 ]
