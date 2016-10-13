18c4ab4299d8081f5b54eb537f3984871eae7990
riv <roller145@yandex.ru>
2016-10-13 14:26:55 +0300

    rest permissions for pet

--- pet/serializers.py
+++ pet/serializers.py
@@ -4,6 +4,8 @@ from models import Pet
 
 
 class PetSerializer(serializers.HyperlinkedModelSerializer):
+    author = serializers.ReadOnlyField(source='author.id')
+    
     class Meta:
         model = Pet
         fields = ('author', 'date_created', 'birthday', 'name', 'species', 'avatar')
--- pet/views.py
+++ pet/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 from pet.models import Pet
+from pet.permissions import IsOwnerOrReadOnly
 from pet.serializers import PetSerializer
+from post import permissions
 
 
 class PetViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
+
+    def get_queryset(self):
+        qs = super(self).get_queryset()
+        if self.request.query_params.get('username'):
+            qs = qs.filter(author__username=self.request.query_params.get('username'))
+        return qs
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
